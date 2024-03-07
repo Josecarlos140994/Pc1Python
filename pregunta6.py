77d26157395c3de6f5bc390005a9db02a0dd7100
Josecarlos140994 <162035043+Josecarlos140994@users.noreply.github.com>
2024-03-07 15:17:55 +0000

    solucion problema 6

---
+++ pregunta6.py
@@ -0,0 +1,16 @@
+##Escribir un programa para una empresa que tiene salas de juegos para todas las edades y quiere
+##calcular de forma automática el precio que debe cobrar a sus clientes por entrar. El programa debe
+##preguntar al usuario la edad del cliente y mostrar el precio de la entrada. Si el cliente es menor de 4
+##años puede entrar gratis, si tiene entre 4 y 18 años debe pagar $5 y si es mayor de 18 años, S10.
+
+
+Edad = int(input("Cual es su edad tienes?:"))
+
+if Edad < 4:
+    Entrada = str("Gratis")
+elif Edad <= 18:
+    Entrada = str("$5")
+else:
+    Entrada = str("$10")
+
+print(f"El precio de la entrada es {Entrada}")
