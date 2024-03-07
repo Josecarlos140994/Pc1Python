73500946e97c9e46f22e9d5d178981430b72fefe
Josecarlos140994 <162035043+Josecarlos140994@users.noreply.github.com>
2024-03-07 14:12:59 +0000

    solucion problema 4

---
+++ pregunta4.py
@@ -0,0 +1,10 @@
+
+
+N = int(input("Introducir un entero positivo, N: "))
+
+if N > 0:
+    sumar = N*(N+1)/2
+    print(f"La suma de los {N} primeros enteros positivos es {sumar}")
+
+else:
+    print (f"introduzca un numero positivo porfavor.")
